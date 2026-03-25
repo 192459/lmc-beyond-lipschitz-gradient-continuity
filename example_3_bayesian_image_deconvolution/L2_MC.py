c659625c4245d65ddca3f39c6161ac244e91613a
192459 <108140456+192459@users.noreply.github.com>
2026-03-25 23:24:52 +0100

    Added comment to L2_MC class

--- example_3_bayesian_image_deconvolution/L2_MC.py
+++ example_3_bayesian_image_deconvolution/L2_MC.py
@@ -9,87 +9,20 @@ from pyproximal.ProxOperator import _check_tau
 from pyproximal import ProxOperator
 
 class L2_MC(ProxOperator):
-    r"""L2 Norm proximal operator.
+    r"""
+    This is customized pyproximal.L2 operator with modified method prox
+    L2 Norm proximal operator to be able to calculate the following one:
 
-    The Proximal operator of the :math:`\ell_2` norm is defined as: :math:`f(\mathbf{x}) =
-    \frac{\sigma}{2} ||\mathbf{Op}\mathbf{x} - \mathbf{b}||_2^2`
-    and :math:`f_\alpha(\mathbf{x}) = f(\mathbf{x}) +
-    \alpha \mathbf{q}^T\mathbf{x}`.
+    `f(\mathbf{x}) =
+    \frac{\sigma}{2} ||\mathbf{Op}\mathbf{x} - \mathbf{b}||_2^2 
+    + 1/2\time_step ||\mathbf{x} - \mathbf{v}||^2`
+    
+    # !!!! in comparison to the original pyproximal.L2 they are added two more 
+    # parameters:
 
-    Parameters
-    ----------
-    Op : :obj:`pylops.LinearOperator`, optional
-        Linear operator
-    b : :obj:`numpy.ndarray`, optional
+    v : :obj:`numpy.ndarray`, optional
         Data vector
-    q : :obj:`numpy.ndarray`, optional
-        Dot vector
-    sigma : :obj:`int`, optional
-        Multiplicative coefficient of L2 norm
-    alpha : :obj:`float`, optional
-        Multiplicative coefficient of dot product
-    qgrad : :obj:`bool`, optional
-        Add q term to gradient (``True``) or not (``False``)
-    niter : :obj:`int` or :obj:`func`, optional
-        Number of iterations of iterative scheme used to compute the proximal.
-        This can be a constant number or a function that is called passing a
-        counter which keeps track of how many times the ``prox`` method has
-        been invoked before and returns the ``niter`` to be used.
-    x0 : :obj:`np.ndarray`, optional
-        Initial vector
-    warm : :obj:`bool`, optional
-        Warm start (``True``) or not (``False``). Uses estimate from previous
-        call of ``prox`` method.
-    densesolver : :obj:`str`, optional
-        Use ``numpy``, ``scipy``, or ``factorize`` when dealing with explicit
-        operators. The former two rely on dense solvers from either library,
-        whilst the last computes a factorization of the matrix to invert and
-        avoids to do so unless the :math:`\tau` or :math:`\sigma` paramets
-        have changed. Choose ``densesolver=None`` when using PyLops versions
-        earlier than v1.18.1 or v2.0.0
-    **kwargs_solver : :obj:`dict`, optional
-        Dictionary containing extra arguments for
-        :py:func:`scipy.sparse.linalg.lsqr` solver when using
-        numpy data (or :py:func:`pylops.optimization.solver.lsqr` and
-        when using cupy data)
-
-    Notes
-    -----
-    The L2 proximal operator is defined as:
-
-    .. math::
-
-        prox_{\tau f_\alpha}(\mathbf{x}) =
-        \left(\mathbf{I} + \tau \sigma \mathbf{Op}^T \mathbf{Op} \right)^{-1}
-        \left( \mathbf{x} + \tau \sigma \mathbf{Op}^T \mathbf{b} -
-        \tau \alpha \mathbf{q}\right)
-
-    when both ``Op`` and ``b`` are provided. This formula shows that the
-    proximal operator requires the solution of an inverse problem. If the
-    operator ``Op`` is of kind ``explicit=True``, we can solve this problem
-    directly. On the other hand if ``Op`` is of kind ``explicit=False``, an
-    iterative solver is employed. In this case it is possible to provide a warm
-    start via the ``x0`` input parameter.
-
-    When only ``b`` is provided, ``Op`` is assumed to be an Identity operator
-    and the proximal operator reduces to:
-
-    .. math::
-
-        \prox_{\tau f_\alpha}(\mathbf{x}) =
-        \frac{\mathbf{x} + \tau \sigma \mathbf{b} - \tau \alpha \mathbf{q}}
-        {1 + \tau \sigma}
-
-    If ``b`` is not provided, the proximal operator reduces to:
-
-    .. math::
-
-        \prox_{\tau f_\alpha}(\mathbf{x}) =
-        \frac{\mathbf{x} - \tau \alpha \mathbf{q}}{1 + \tau \sigma}
-
-    Finally, note that the second term in :math:`f_\alpha(\mathbf{x})` is added
-    because this combined expression appears in several problems where Bregman
-    iterations are used alongside a proximal solver.
+    time_step : `np.float`, optional
 
     """
     def __init__(self, Op=None, b=None, v=None, time_step=None, sigma=1., 
